ba3608db71a1f20d86a84c023c01288232a68c38
mdmills1990@gmail.com <mdmills1990@gmail.com>
2018-07-02 22:16:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function logWhisper(string){
   return string.toLowerCase()
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase('hello'))
+  if (string.toLowerCase())
   return ("I can't hear you!")
-  else if (string.toUpperCase('HELLO'))
+  else if (string.toUpperCase())
   return ("YES INDEED!")
   else return ("I love you, too.")
 }
