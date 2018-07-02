aaacb67fe699fd56a79ceba275fe301345667226
mdmills1990@gmail.com <mdmills1990@gmail.com>
2018-07-02 21:54:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string){
+  console.log('hello')
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string){
+  if (console.log('hello'))
+  return ("I can't hear you!")
+  else if (console.log('HELLO'))
+  return ("YES INDEED!")
+  else return ("I love you, too.")
+}
